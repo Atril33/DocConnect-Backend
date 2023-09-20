373ec392f2ccc9d5d4200047103494420f103c19
david <andrianarivo.david@gmail.com>
2023-09-20 22:16:49 +0300

    Refactor SessionsController for unauthorized

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,10 +6,15 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render json: {
-      status: { code: 200, message: 'Logged in sucessfully.' },
-      data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-    }, status: :ok
+    if current_user
+      render json: {
+        status: { code: 200, message: 'Logged in sucessfully.' },
+        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }, status: :ok
+    else
+      render_unauthorized
+    end
+
   end
 
   def respond_to_on_destroy
@@ -19,10 +24,14 @@ class Users::SessionsController < Devise::SessionsController
         message: "logged out successfully"
       }, status: :ok
     else
-      render json: {
-        status: 401,
-        message: "Couldn't find an active session."
-      }, status: :unauthorized
+      render_unauthorized
     end
+  end
+
+  def render_unauthorized
+    render json: {
+      status: 401,
+      message: "Couldn't find an active session."
+    }, status: :unauthorized
   end
 end
