64b5f455a7e2239ae78f43bc4f418f4ef9666418
david <andrianarivo.david@gmail.com>
2023-09-20 22:23:38 +0300

    Permit name parameters on SignUp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
   include RackSessionFix
+  before_action :configure_devise_params, if: :devise_controller?
+
+  def configure_devise_params
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :created_at, :jti
+  attributes :id, :email, :created_at
 end
