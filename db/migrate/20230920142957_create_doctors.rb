7dcd53f3158334f598176d2c8f7d0544be9c9a2f
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-09-27 00:09:47 +0500

    updating doctor schema

--- db/migrate/20230920142957_create_doctors.rb
+++ db/migrate/20230920142957_create_doctors.rb
@@ -2,8 +2,8 @@ class CreateDoctors < ActiveRecord::Migration[6.0]
   def change
     create_table :doctors do |t|
       t.string :name
-      t.date :time_available_from
-      t.date :time_available_to
+      t.time :time_available_from
+      t.time :time_available_to
       t.text :bio
       t.decimal :fee_per_appointment
       t.references :specialization, foreign_key: true
