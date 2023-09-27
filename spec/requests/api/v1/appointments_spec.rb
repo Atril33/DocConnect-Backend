e179425c4a78aad6b7df6b52993f771b07bfa2d3
andrianarivo <andrianarivo.david@gmail.com>
2023-09-27 16:13:52 +0300

    Fix rubocop offenses

--- spec/requests/api/v1/appointments_spec.rb
+++ spec/requests/api/v1/appointments_spec.rb
@@ -1,12 +1,9 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/appointments', type: :request do
-
   path '/api/v1/appointments' do
-
     get('list appointments') do
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -20,7 +17,6 @@ RSpec.describe 'api/v1/appointments', type: :request do
 
     post('create appointment') do
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -34,25 +30,9 @@ RSpec.describe 'api/v1/appointments', type: :request do
   end
 
   path '/api/v1/appointments/{id}' do
-    # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
     get('show appointment') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    patch('update appointment') do
       response(200, 'successful') do
         let(:id) { '123' }
 
