b4268a2c05059e91c6ade1751903523e00aa6ad3
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-10-03 01:40:53 +0500

    fixing

--- spec/requests/specializations_spec.rb
+++ spec/requests/specializations_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe 'Specializations', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/specializations/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
+
 end
