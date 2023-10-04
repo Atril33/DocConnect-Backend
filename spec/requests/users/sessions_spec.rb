348787748fabb68bd753ddfc5d990f8cdf893178
lordksix <121689749+lordksix@users.noreply.github.com>
2023-10-04 06:40:30 -0500

    Fix registration specs

--- spec/requests/users/sessions_spec.rb
+++ spec/requests/users/sessions_spec.rb
@@ -1,6 +1,41 @@
 require 'swagger_helper'
+require 'rails_helper'
 
-# rubocop:disable Lint/EmptyBlock
 RSpec.describe 'users/sessions', type: :request do
+  describe 'POST/SIGNIN' do
+    context 'with valid parameters' do
+      before do
+        post '/signup', params:
+                          { user: {
+                            name: 'tester_222',
+                            email: 'mail555@mail.com',
+                            password: '12345678',
+                            password_confirmation: '12345678'
+                          } }
+        post '/login', params:
+                          { user: {
+                            email: 'mail555@mail.com',
+                            password: '12345678'
+                          } }
+      end
+
+      it 'returns a found status' do
+        expect(response).to have_http_status(:found)
+      end
+    end
+
+    context 'with invalid parameters password' do
+      before do
+        post '/login', params:
+                          { user: {
+                            email: nil,
+                            password: '12345678'
+                          } }
+      end
+
+      it 'returns an unprocessable entity' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 end
-# rubocop:enable Lint/EmptyBlock
